3877d383a46ecbd168e8041d3fdc2698959a3ed7
Ethan <christian.deguzman@pointwest.com.ph>
2024-02-07 15:04:25 +0800

    added line-area-chart

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ChartComponent } from "./chart/chart.component";
+import { LineChartComponent } from './line-chart/line-chart.component';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, ChartComponent, LineChartComponent]
 })
 export class AppComponent {
   title = 'test-app';
