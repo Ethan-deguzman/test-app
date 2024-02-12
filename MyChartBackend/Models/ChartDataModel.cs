dacba450831829d5c6a303b65338b94cbbd4712d
Ethan <christian.deguzman@pointwest.com.ph>
2024-02-13 00:11:49 +0800

    modified backend

--- MyChartBackend/Models/ChartDataModel.cs
+++ MyChartBackend/Models/ChartDataModel.cs
@@ -1,12 +1,12 @@
 public class ChartData
 {
-	public string[] Labels { get; set; }
-	public ChartDataset[] Datasets { get; set; }
+	public string[] ?Labels { get; set; }
+	public ChartDataset[] ?Datasets { get; set; } 
 }
 
 public class ChartDataset
 {
-	public string Label { get; set; }
-	public int[] Data { get; set; }
+	public string ?Label { get; set; } 
+	public int[] ?Data { get; set; } 
 	
 }
--- MyChartBackend/obj/Debug/net7.0/MyChartBackend.AssemblyInfo.cs
+++ MyChartBackend/obj/Debug/net7.0/MyChartBackend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyChartBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aae75c6c0f7341285f6e15988ed4bbbcec2721e6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82a9e40e8f8431fb7537806e6299bacd07379fdf")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyChartBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyChartBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
